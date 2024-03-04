2f9cd43fa82511fe95d8d61d7a0e88fc8deb3387
Aditipradhan123 <109887476+Aditipradhan123@users.noreply.github.com>
2024-03-04 17:23:29 +0545

    push

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,4 @@
-'use client'
-// import Link from 'next/link'
-// import { useState } from 'react';
+import { useState } from 'react';
 import { BiSolidDashboard, BiDetail, BiSolidBriefcase, BiTask, BiUser, BiLogIn } from "react-icons/bi";
 import { BsPersonPlus } from "react-icons/bs";
 import { usePathname } from "next/navigation";
@@ -64,23 +62,45 @@ const sidebarItems = [
 ]
 
 const Sidebar = ({isMenuOpen}) => {
-  const pathname =usePathname()
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const pathname = usePathname();
+
+  const handleDropdown = (index) => {
+    setOpenDropdown(index === openDropdown ? null : index);
+  };
+
   return (
     <aside className="fixed h-screen bg-black mt-14 p-4">
-    <ul  className=" text-gray-200">
-      {sidebarItems.map(({ name, href, icon: Icon, subItems}) => (
-      <li key={name} className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer ${pathname === href ? 'bg-gray-700 ' : 'hover:bg-gray-700'}
-      `}>
-        <span >
-          <Icon size={24} />
-        </span>
-        <span className={`w-52 ml-3  ${!isMenuOpen ? 'hidden' : 'block'}`}>{name}</span>
-      </li>
-      ))}
-    </ul>
+      <ul className="text-gray-200">
+        {sidebarItems.map(({ name, href, icon: Icon, subItems }, index) => (
+          <li key={name} className={`relative ${subItems ? 'group' : ''}`}>
+            <div
+              className={`flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer ${
+                pathname === href ? 'bg-gray-700' : 'hover:bg-gray-700'
+              }`}
+              onClick={() => subItems && handleDropdown(index)}
+            >
+              <span>
+                <Icon size={24} />
+              </span>
+              <span className={`w-52 ml-3 ${!isMenuOpen ? 'hidden' : 'block'}`}>{name}</span>
+            </div>
+            {subItems && openDropdown === index && (
+              <ul className="">
+                {subItems.map(({ name: subName, href: subHref }) => (
+                  <li key={subName} className={`flex text-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer ${
+                    pathname === subHref ? 'bg-gray-700' : 'hover:bg-gray-700'
+                  }`}>
+                    <span>{subName}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
     </aside>
-  )
-}
+  );
+};
 
-
-export default Sidebar
+export default Sidebar;

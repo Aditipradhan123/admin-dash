6e9894d71950bb1ddb7caf4d9d9c1c047e797557
Aditipradhan123 <109887476+Aditipradhan123@users.noreply.github.com>
2024-03-04 17:01:35 +0545

    send

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,6 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
- 
-
   return (
     <html lang="en">
       <body className={inter.className}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,27 +6,23 @@ import Sidebar from './Sidebar'
 
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(true); // State to manage menu open/close
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
   const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen); // Toggle the menu state
+    setIsMenuOpen(!isMenuOpen);
   };
   return (
     <div>
-    <nav className='fixed bg-black text-gray-300 w-full h-16 shadow-2xl pt-3 pl-4'>
+    <nav className='fixed bg-black text-gray-300 w-full h-16 shadow-2xl pt-3 pl-4 z-10'>
         <div className='sidebar_top flex gap-4 text-xl uppercase font-sans items-center'>
         <Image alt='' src='/logo.png' width={35} height={35} />
         <span>Admin</span>
         <button className='p-1 ml-36 rounded-lg' onClick={handleMenuToggle}>
-          {isMenuOpen ? <AiOutlineMenu size={32} /> : <AiOutlineMenu size={32} />}
+        <AiOutlineMenu size={32}  />
         </button>
       </div>
     </nav>
-    {isMenuOpen && (
-        <div >
-          <Sidebar/>
-        </div>
-      )}
+    <Sidebar isMenuOpen={isMenuOpen}/>
     </div>
   )
 }
